4fd33aac6443d43381e93bf036cf7f4d15132a49
Eva <orakle@tutamail.com>
2023-05-04 15:52:57 +0200

    Rework du formulaire des options (affichage de la valeur courante, redesign)

--- SAE-S2-01-MasterMind/Form_Modif_Options.Designer.vb
+++ SAE-S2-01-MasterMind/Form_Modif_Options.Designer.vb
@@ -30,96 +30,160 @@ Partial Class Form_Modif_Options
         Me.btn_modif_symb_5 = New System.Windows.Forms.Button()
         Me.lbl_modif_symb_def = New System.Windows.Forms.Label()
         Me.Btn_modif_temps = New System.Windows.Forms.Button()
-        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lbl_symbole1 = New System.Windows.Forms.Label()
+        Me.lbl_symbole2 = New System.Windows.Forms.Label()
+        Me.lbl_symbole3 = New System.Windows.Forms.Label()
+        Me.lbl_symbole4 = New System.Windows.Forms.Label()
+        Me.lbl_symbole5 = New System.Windows.Forms.Label()
+        Me.lbl_temps = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'btn_quitter
         '
-        Me.btn_quitter.Location = New System.Drawing.Point(388, 15)
+        Me.btn_quitter.Location = New System.Drawing.Point(289, 121)
+        Me.btn_quitter.Margin = New System.Windows.Forms.Padding(2)
         Me.btn_quitter.Name = "btn_quitter"
-        Me.btn_quitter.Size = New System.Drawing.Size(103, 75)
+        Me.btn_quitter.Size = New System.Drawing.Size(74, 24)
         Me.btn_quitter.TabIndex = 0
         Me.btn_quitter.Text = "Quitter"
         Me.btn_quitter.UseVisualStyleBackColor = True
         '
         'btn_modif_symb_1
         '
-        Me.btn_modif_symb_1.Location = New System.Drawing.Point(12, 37)
+        Me.btn_modif_symb_1.Location = New System.Drawing.Point(99, 27)
+        Me.btn_modif_symb_1.Margin = New System.Windows.Forms.Padding(2)
         Me.btn_modif_symb_1.Name = "btn_modif_symb_1"
-        Me.btn_modif_symb_1.Size = New System.Drawing.Size(99, 23)
+        Me.btn_modif_symb_1.Size = New System.Drawing.Size(74, 24)
         Me.btn_modif_symb_1.TabIndex = 1
-        Me.btn_modif_symb_1.Text = "Symbole 1"
+        Me.btn_modif_symb_1.Text = "Modifier"
         Me.btn_modif_symb_1.UseVisualStyleBackColor = True
         '
         'btn_modif_symb_2
         '
-        Me.btn_modif_symb_2.Location = New System.Drawing.Point(12, 67)
+        Me.btn_modif_symb_2.Location = New System.Drawing.Point(289, 27)
+        Me.btn_modif_symb_2.Margin = New System.Windows.Forms.Padding(2)
         Me.btn_modif_symb_2.Name = "btn_modif_symb_2"
-        Me.btn_modif_symb_2.Size = New System.Drawing.Size(99, 23)
+        Me.btn_modif_symb_2.Size = New System.Drawing.Size(74, 24)
         Me.btn_modif_symb_2.TabIndex = 2
-        Me.btn_modif_symb_2.Text = "Symbole 2"
+        Me.btn_modif_symb_2.Text = "Modifier"
         Me.btn_modif_symb_2.UseVisualStyleBackColor = True
         '
         'btn_modif_symb_3
         '
-        Me.btn_modif_symb_3.Location = New System.Drawing.Point(12, 97)
+        Me.btn_modif_symb_3.Location = New System.Drawing.Point(98, 55)
+        Me.btn_modif_symb_3.Margin = New System.Windows.Forms.Padding(2)
         Me.btn_modif_symb_3.Name = "btn_modif_symb_3"
-        Me.btn_modif_symb_3.Size = New System.Drawing.Size(99, 23)
+        Me.btn_modif_symb_3.Size = New System.Drawing.Size(74, 24)
         Me.btn_modif_symb_3.TabIndex = 3
-        Me.btn_modif_symb_3.Text = "Symbole 3"
+        Me.btn_modif_symb_3.Text = "Modifier"
         Me.btn_modif_symb_3.UseVisualStyleBackColor = True
         '
         'btn_modif_symb_4
         '
-        Me.btn_modif_symb_4.Location = New System.Drawing.Point(12, 127)
+        Me.btn_modif_symb_4.Location = New System.Drawing.Point(289, 55)
+        Me.btn_modif_symb_4.Margin = New System.Windows.Forms.Padding(2)
         Me.btn_modif_symb_4.Name = "btn_modif_symb_4"
-        Me.btn_modif_symb_4.Size = New System.Drawing.Size(99, 23)
+        Me.btn_modif_symb_4.Size = New System.Drawing.Size(74, 24)
         Me.btn_modif_symb_4.TabIndex = 4
-        Me.btn_modif_symb_4.Text = "Symbole 4"
+        Me.btn_modif_symb_4.Text = "Modifier"
         Me.btn_modif_symb_4.UseVisualStyleBackColor = True
         '
         'btn_modif_symb_5
         '
-        Me.btn_modif_symb_5.Location = New System.Drawing.Point(12, 157)
+        Me.btn_modif_symb_5.Location = New System.Drawing.Point(99, 83)
+        Me.btn_modif_symb_5.Margin = New System.Windows.Forms.Padding(2)
         Me.btn_modif_symb_5.Name = "btn_modif_symb_5"
-        Me.btn_modif_symb_5.Size = New System.Drawing.Size(99, 23)
+        Me.btn_modif_symb_5.Size = New System.Drawing.Size(74, 24)
         Me.btn_modif_symb_5.TabIndex = 5
-        Me.btn_modif_symb_5.Text = "Symbole 5"
+        Me.btn_modif_symb_5.Text = "Modifier"
         Me.btn_modif_symb_5.UseVisualStyleBackColor = True
         '
         'lbl_modif_symb_def
         '
         Me.lbl_modif_symb_def.AutoSize = True
-        Me.lbl_modif_symb_def.Location = New System.Drawing.Point(12, 15)
+        Me.lbl_modif_symb_def.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_modif_symb_def.Location = New System.Drawing.Point(9, 9)
+        Me.lbl_modif_symb_def.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.lbl_modif_symb_def.Name = "lbl_modif_symb_def"
-        Me.lbl_modif_symb_def.Size = New System.Drawing.Size(230, 16)
+        Me.lbl_modif_symb_def.Size = New System.Drawing.Size(151, 13)
         Me.lbl_modif_symb_def.TabIndex = 6
-        Me.lbl_modif_symb_def.Text = "Modification des symboles par défaut"
+        Me.lbl_modif_symb_def.Text = "Modifications des options"
         '
         'Btn_modif_temps
         '
-        Me.Btn_modif_temps.Location = New System.Drawing.Point(12, 206)
+        Me.Btn_modif_temps.Location = New System.Drawing.Point(98, 121)
+        Me.Btn_modif_temps.Margin = New System.Windows.Forms.Padding(2)
         Me.Btn_modif_temps.Name = "Btn_modif_temps"
-        Me.Btn_modif_temps.Size = New System.Drawing.Size(75, 23)
+        Me.Btn_modif_temps.Size = New System.Drawing.Size(74, 24)
         Me.Btn_modif_temps.TabIndex = 7
         Me.Btn_modif_temps.Text = "Modifier"
         Me.Btn_modif_temps.UseVisualStyleBackColor = True
         '
-        'Label1
+        'lbl_symbole1
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(13, 187)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(305, 16)
-        Me.Label1.TabIndex = 8
-        Me.Label1.Text = "Modification du temps pour trouver la combinaison"
+        Me.lbl_symbole1.AutoSize = True
+        Me.lbl_symbole1.Location = New System.Drawing.Point(9, 33)
+        Me.lbl_symbole1.Name = "lbl_symbole1"
+        Me.lbl_symbole1.Size = New System.Drawing.Size(71, 13)
+        Me.lbl_symbole1.TabIndex = 9
+        Me.lbl_symbole1.Text = "Symbole 1 : []"
+        '
+        'lbl_symbole2
+        '
+        Me.lbl_symbole2.AutoSize = True
+        Me.lbl_symbole2.Location = New System.Drawing.Point(199, 33)
+        Me.lbl_symbole2.Name = "lbl_symbole2"
+        Me.lbl_symbole2.Size = New System.Drawing.Size(71, 13)
+        Me.lbl_symbole2.TabIndex = 9
+        Me.lbl_symbole2.Text = "Symbole 2 : []"
+        '
+        'lbl_symbole3
+        '
+        Me.lbl_symbole3.AutoSize = True
+        Me.lbl_symbole3.Location = New System.Drawing.Point(9, 61)
+        Me.lbl_symbole3.Name = "lbl_symbole3"
+        Me.lbl_symbole3.Size = New System.Drawing.Size(71, 13)
+        Me.lbl_symbole3.TabIndex = 9
+        Me.lbl_symbole3.Text = "Symbole 3 : []"
+        '
+        'lbl_symbole4
+        '
+        Me.lbl_symbole4.AutoSize = True
+        Me.lbl_symbole4.Location = New System.Drawing.Point(200, 61)
+        Me.lbl_symbole4.Name = "lbl_symbole4"
+        Me.lbl_symbole4.Size = New System.Drawing.Size(71, 13)
+        Me.lbl_symbole4.TabIndex = 9
+        Me.lbl_symbole4.Text = "Symbole 4 : []"
+        '
+        'lbl_symbole5
+        '
+        Me.lbl_symbole5.AutoSize = True
+        Me.lbl_symbole5.Location = New System.Drawing.Point(10, 89)
+        Me.lbl_symbole5.Name = "lbl_symbole5"
+        Me.lbl_symbole5.Size = New System.Drawing.Size(71, 13)
+        Me.lbl_symbole5.TabIndex = 9
+        Me.lbl_symbole5.Text = "Symbole 5 : []"
+        '
+        'lbl_temps
+        '
+        Me.lbl_temps.AutoSize = True
+        Me.lbl_temps.Location = New System.Drawing.Point(9, 127)
+        Me.lbl_temps.Name = "lbl_temps"
+        Me.lbl_temps.Size = New System.Drawing.Size(69, 13)
+        Me.lbl_temps.TabIndex = 10
+        Me.lbl_temps.Text = "Temps : 1:30"
         '
         'Form_Modif_Options
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(503, 438)
-        Me.Controls.Add(Me.Label1)
+        Me.ClientSize = New System.Drawing.Size(378, 161)
+        Me.Controls.Add(Me.lbl_temps)
+        Me.Controls.Add(Me.lbl_symbole5)
+        Me.Controls.Add(Me.lbl_symbole4)
+        Me.Controls.Add(Me.lbl_symbole3)
+        Me.Controls.Add(Me.lbl_symbole2)
+        Me.Controls.Add(Me.lbl_symbole1)
         Me.Controls.Add(Me.Btn_modif_temps)
         Me.Controls.Add(Me.lbl_modif_symb_def)
         Me.Controls.Add(Me.btn_modif_symb_5)
@@ -128,8 +192,10 @@ Partial Class Form_Modif_Options
         Me.Controls.Add(Me.btn_modif_symb_2)
         Me.Controls.Add(Me.btn_modif_symb_1)
         Me.Controls.Add(Me.btn_quitter)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "Form_Modif_Options"
-        Me.Text = "ModifOptions"
+        Me.Text = "Modification des options"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -143,5 +209,10 @@ Partial Class Form_Modif_Options
     Friend WithEvents btn_modif_symb_5 As Button
     Friend WithEvents lbl_modif_symb_def As Label
     Friend WithEvents Btn_modif_temps As Button
-    Friend WithEvents Label1 As Label
+    Friend WithEvents lbl_symbole1 As Label
+    Friend WithEvents lbl_symbole2 As Label
+    Friend WithEvents lbl_symbole3 As Label
+    Friend WithEvents lbl_symbole4 As Label
+    Friend WithEvents lbl_symbole5 As Label
+    Friend WithEvents lbl_temps As Label
 End Class
--- SAE-S2-01-MasterMind/Form_Modif_Options.vb
+++ SAE-S2-01-MasterMind/Form_Modif_Options.vb
@@ -1,6 +1,19 @@
 ﻿Public Class Form_Modif_Options
-    Private Sub Form_Modif_Options_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    Private Sub Change_Label_Temps()
+        lbl_temps.Text = "Temps : " & GetTemps().Minutes & ":" & GetTemps().Seconds
+        If (GetTemps().Seconds < 10) Then
+            lbl_temps.Text = lbl_temps.Text & "0"
+        End If
+    End Sub
+
+    Private Sub Form_Modif_Options_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lbl_symbole1.Text = "Symbole 1 : " & GetSymbDef1()
+        lbl_symbole2.Text = "Symbole 2 : " & GetSymbDef2()
+        lbl_symbole3.Text = "Symbole 3 : " & GetSymbDef3()
+        lbl_symbole5.Text = "Symbole 5 : " & GetSymbDef4()
+        lbl_symbole4.Text = "Symbole 4 : " & GetSymbDef5()
+        Change_Label_Temps()
     End Sub
     Private Sub Form_Modif_Options_FormClosed(sender As Object, e As FormClosedEventArgs) Handles Me.FormClosed
         Form_Accueil.Show()
@@ -14,31 +27,57 @@
     Private Sub Btn_modif_symb_1_Click(sender As Object, e As EventArgs) Handles btn_modif_symb_1.Click
         Dim rep As String = InputBox("Nouveau Symbole : ", "Changement de symbole", Symboles.GetSymbDef1)
         If Not rep = "" Then
-            Symboles.SetSymbDef1(rep)
+            If rep.Length = 1 Then
+                Symboles.SetSymbDef1(rep)
+                lbl_symbole1.Text = "Symbole 1 : " & GetSymbDef1()
+            Else
+                MsgBox("Le symbole renseigné est trop long. Il ne doit pas dépasser un caractère.", MsgBoxStyle.OkOnly)
+            End If
         End If
     End Sub
     Private Sub Btn_modif_symb_2_Click(sender As Object, e As EventArgs) Handles btn_modif_symb_2.Click
         Dim rep As String = InputBox("Nouveau Symbole : ", "Changement de symbole", Symboles.GetSymbDef2)
         If Not rep = "" Then
-            Symboles.SetSymbDef2(rep)
+            If rep.Length = 1 Then
+                Symboles.SetSymbDef2(rep)
+                lbl_symbole2.Text = "Symbole 2 : " & GetSymbDef2()
+            Else
+                MsgBox("Le symbole renseigné est trop long. Il ne doit pas dépasser un caractère.", MsgBoxStyle.OkOnly)
+            End If
+
         End If
     End Sub
     Private Sub Btn_modif_symb_3_Click(sender As Object, e As EventArgs) Handles btn_modif_symb_3.Click
         Dim rep As String = InputBox("Nouveau Symbole : ", "Changement de symbole", Symboles.GetSymbDef3)
         If Not rep = "" Then
-            Symboles.SetSymbDef3(rep)
+            If rep.Length = 1 Then
+                Symboles.SetSymbDef3(rep)
+                lbl_symbole3.Text = "Symbole 3 : " & GetSymbDef3()
+            Else
+                MsgBox("Le symbole renseigné est trop long. Il ne doit pas dépasser un caractère.", MsgBoxStyle.OkOnly)
+            End If
         End If
     End Sub
     Private Sub Btn_modif_symb_4_Click(sender As Object, e As EventArgs) Handles btn_modif_symb_4.Click
         Dim rep As String = InputBox("Nouveau Symbole : ", "Changement de symbole", Symboles.GetSymbDef4)
         If Not rep = "" Then
-            Symboles.SetSymbDef4(rep)
+            If rep.Length = 1 Then
+                Symboles.SetSymbDef4(rep)
+                lbl_symbole4.Text = "Symbole 4 : " & GetSymbDef4()
+            Else
+                MsgBox("Le symbole renseigné est trop long. Il ne doit pas dépasser un caractère.", MsgBoxStyle.OkOnly)
+            End If
         End If
     End Sub
     Private Sub Btn_modif_symb_5_Click(sender As Object, e As EventArgs) Handles btn_modif_symb_5.Click
         Dim rep As String = InputBox("Nouveau Symbole : ", "Changement de symbole", Symboles.GetSymbDef5)
-        If (Not rep = "") Then
-            Symboles.SetSymbDef5(rep)
+        If Not rep = "" Then
+            If rep.Length = 1 Then
+                Symboles.SetSymbDef5(rep)
+                lbl_symbole5.Text = "Symbole 5 : " & GetSymbDef5()
+            Else
+                MsgBox("Le symbole renseigné est trop long. Il ne doit pas dépasser un caractère.", MsgBoxStyle.OkOnly)
+            End If
         End If
     End Sub
 
@@ -51,6 +90,7 @@
         Else
             Dim temps As New TimeSpan(0, min, sec)
             LeTemps.UpdateTemps(temps)
+            Change_Label_Temps()
         End If
     End Sub
 End Class
