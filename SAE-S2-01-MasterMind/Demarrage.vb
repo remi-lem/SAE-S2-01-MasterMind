1434466f2c1f41667046797f0502423bc41cb3ff
Rémi L <94972054+remi-lem@users.noreply.github.com>
2023-05-11 22:27:27 +0200

    enleve le mode debug + purge registre.txt + test x1

--- SAE-S2-01-MasterMind/Demarrage.vb
+++ SAE-S2-01-MasterMind/Demarrage.vb
@@ -1,5 +1,5 @@
 ﻿Module Demarrage
-    Public DEBUG_MODE = True ' METTRE FALSE AVANT DE RENDRE !!!!
+    Public DEBUG_MODE = False ' mettre True pour des tests plus rapides
 
     Sub Main()
         Dim nombre As Integer = 20
